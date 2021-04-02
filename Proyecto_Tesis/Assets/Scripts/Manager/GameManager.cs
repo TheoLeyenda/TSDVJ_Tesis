03aacc42a9493c38fde032af2a34c58c9c887186
TheoLeyenda <31700515+TheoLeyenda@users.noreply.github.com>
2021-04-02 13:59:31 -0300

    Importing TMP and GameOver Function

--- Proyecto_Tesis/Assets/Scripts/Manager/GameManager.cs
+++ Proyecto_Tesis/Assets/Scripts/Manager/GameManager.cs
@@ -1,18 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void GameOver(string nameGameOver)
     {
-        
+        SceneManager.LoadScene(nameGameOver);
     }
-
-    // Update is called once per frame
-    void Update()
+    public void ExitGame()
     {
-        
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
     }
 }

efbd430ffc9afbf2f3363e1efad69bc2c487c4d4
José Quintana Martín <jquintanamartin@hotmail.com>
2021-04-03 11:33:04 -0300

    Added simple mouse input

---
+++ Proyecto_Tesis/Assets/Jose Quintana Martin/J_MouseInput.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class J_MouseInput : MonoBehaviour
+{
+    public Vector2 turn;
+
+    public float sensitivity = 2f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        turn.x += Input.GetAxis("Mouse X") * sensitivity;
+        turn.y += Input.GetAxis("Mouse Y") * sensitivity;
+
+        if (turn.y > 90)
+        {
+            turn.y = 90;
+        }
+
+        if (turn.y < -90)
+        {
+            turn.y = -90;
+        }
+
+        transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
+    }
+}

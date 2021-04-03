8acb4999692aafea0928dd15c72eda2a30c5d6bb
José Quintana Martín <jquintanamartin@hotmail.com>
2021-04-03 10:59:44 -0300

    Player Moves using rb.velocity instead of transform.position

---
+++ Proyecto_Tesis/Assets/Jose Quintana Martin/J_Movement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class J_Movement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void HorizontalMove(bool isPositve)
+    {
+        float xMov;
+
+        if (isPositve)
+            xMov = 1 * speed * Time.deltaTime;
+        else
+            xMov = -1 * speed * Time.deltaTime;
+
+        rb.velocity = new Vector3(xMov, rb.velocity.y, rb.velocity.z);
+    }
+
+    public void ForwardMove(bool isPositve)
+    {
+        float zMov;
+
+        if (isPositve)
+            zMov = 1 * speed * Time.deltaTime;
+        else
+            zMov = -1 * speed * Time.deltaTime;
+
+        rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, zMov);
+    }
+
+    public void NoInput()
+    {
+        rb.velocity = Vector3.zero;
+    }
+}

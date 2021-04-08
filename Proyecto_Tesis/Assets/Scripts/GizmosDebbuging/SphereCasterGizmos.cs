988b5bf53642eac21306c7743de4f2ee672cd5e0
TheoLeyenda <31700515+TheoLeyenda@users.noreply.github.com>
2021-04-08 15:09:18 -0300

    Update Monster and add class

---
+++ Proyecto_Tesis/Assets/Scripts/GizmosDebbuging/SphereCasterGizmos.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereCasterGizmos : MonoBehaviour
+{
+    [SerializeField] private float maxDistance;
+
+    [SerializeField] float radiusSphere;
+
+    void OnDrawGizmos()
+    {
+        RaycastHit hit;
+
+        bool isHit = Physics.SphereCast(transform.position, radiusSphere,transform.forward, out hit, maxDistance);
+
+        if (isHit)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position + transform.forward * hit.distance, radiusSphere);
+        }
+    }
+}

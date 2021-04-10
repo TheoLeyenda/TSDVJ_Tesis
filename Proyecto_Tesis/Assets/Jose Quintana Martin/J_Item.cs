8a9ff2cdc3e1ab5754068d4fd19fe9eb89fa5f98
José Quintana Martín <jquintanamartin@hotmail.com>
2021-04-10 11:46:54 -0300

    Added Inventory behaviour

---
+++ Proyecto_Tesis/Assets/Jose Quintana Martin/J_Item.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
+public class J_Item : ScriptableObject
+{
+    public string itemName = "New Item";
+    public Sprite icon = null;
+}
---
+++ Proyecto_Tesis/Assets/Jose Quintana Martin/J_ItemPickUp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class J_ItemPickUp : MonoBehaviour
+{
+    public J_Inventory playerInventory;
+    public J_Item item;
+
+    public void AddItemToInventory() {
+        playerInventory.AddItem(item);
+        Debug.Log("Added " + item.itemName + " to inventory");
+
+        Destroy(gameObject);
+    }
+}

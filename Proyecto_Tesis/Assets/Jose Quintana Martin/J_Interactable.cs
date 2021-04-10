40c2a955dbd5008e2d0ca3f309584580284b0e49
José Quintana Martín <jquintanamartin@hotmail.com>
2021-04-10 10:33:35 -0300

    Added Interactable behaviour

---
+++ Proyecto_Tesis/Assets/Jose Quintana Martin/J_Interactable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class J_Interactable : MonoBehaviour
+{
+    [SerializeField] private UnityEvent action;
+
+    public void DoAction()
+    {
+        action.Invoke();
+    }
+}
---
+++ Proyecto_Tesis/Assets/Jose Quintana Martin/J_PlayerInteraction.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class J_PlayerInteraction : MonoBehaviour
+{
+    public GameObject playerCamera;
+    public float interactionDistance = 1f;
+
+    private J_Interactable interactable;
+
+    RaycastHit hit;
+
+    public void Interact()
+    {
+
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, interactionDistance))
+        {
+            interactable = hit.transform.gameObject.GetComponent<J_Interactable>();
+            if (interactable != null)
+                interactable.DoAction();
+            else
+                Debug.Log("Not Ineractable");
+        }
+        else
+            Debug.Log("No collider on range");
+    }
+}

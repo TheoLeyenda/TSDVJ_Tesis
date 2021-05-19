d55635a37842235d38185c8fe5a497bf3b8b480c
theixgamer <theofritaku@gmail.com>
2021-05-19 16:45:44 -0300

    Added Puzzle 4

---
+++ Proyecto_Tesis/Assets/Scripts/Puzzles/DestroyObjectByTime.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObjectByTime : MonoBehaviour
+{
+    public float lifeTime = 5.0f;
+
+    public GameObject objectDestryed;
+
+    private IEnumerator Start()
+    {
+        yield return new WaitForSeconds(lifeTime);
+        Destroy(objectDestryed);
+    }
+}

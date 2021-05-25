978e512e3c72966527bf2040314c39d6992eb5b4
TheoLeyenda <31700515+TheoLeyenda@users.noreply.github.com>
2021-05-25 19:54:32 -0300

    Puzzle 5 en progreso...

---
+++ Proyecto_Tesis/Assets/Scripts/EnableAndDisableObjects.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableAndDisableObjects : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> objectsActivate;
+    [SerializeField] private List<GameObject> objectsDisable;
+
+    [SerializeField] private bool useAutomaticDisable = true;
+
+    public void UpdateObjects()
+    {
+        bool useDisable = true;
+
+        if (useAutomaticDisable)
+        {
+            for (int i = 0; i < objectsActivate.Count; i++)
+            {
+                if (!objectsActivate[i].activeSelf)
+                {
+                    useDisable = false;
+                }
+            }
+        }
+
+        for (int i = 0; i < objectsDisable.Count; i++)
+        {
+            objectsDisable[i].SetActive(false);
+        }
+
+        if (!useDisable)
+        {
+            for (int i = 0; i < objectsActivate.Count; i++)
+            {
+                objectsActivate[i].SetActive(true);
+            }
+        }
+
+        if (useAutomaticDisable && useDisable)
+        {
+            for (int i = 0; i < objectsActivate.Count; i++)
+            {
+                objectsActivate[i].SetActive(false);
+            }
+        }
+    }
+}

b6081636e430359746c3133b7c8454b44d440f88
TheoLeyenda <31700515+TheoLeyenda@users.noreply.github.com>
2021-05-27 01:29:49 -0300

    Puzzle 5 in progres...

--- Proyecto_Tesis/Assets/Scripts/Puzzles/FunctionOnPlaceObjects.cs
+++ Proyecto_Tesis/Assets/Scripts/Puzzles/FunctionOnPlaceObjects.cs
@@ -4,99 +4,175 @@ using UnityEngine.Events;
 
 public class FunctionOnPlaceObjects : MonoBehaviour
 {
+    [SerializeField] private J_Inventory inventory;
+    [SerializeField] private bool useCheckInPlaceObject;
     [System.Serializable]
-    public class ObjectPlace
-    {
+    public class SpawnerItem
+    { 
         [System.Serializable]
-        public class SlotPlace
+        public class Item
         {
-            //Objecto que se ve colocado.
-            public GameObject visibleObject;
-
-            //Objecto Transparentado.
+            public GameObject itemObject;
+            public J_Item item;
             public GameObject transparentObject;
-
-            public int Id_Object;
-
+            public int id_item;
+            public bool itemSpawned = false;
         }
-        public SlotPlace[] arrSlotsPlace;
 
-        //Item que nesesito del jugador.
-        public GameObject compareObject;
+        public bool itemPlace;
+        public SpawnedItem spawnedItem;
 
-        private int currentIndexVisibleObjectOn = -1;
-
-        public void SwitchObjectVisible(int id)
+        public class SpawnedItem
         {
-            GameObject tokenActivated = null;
-            for (int i = 0; i < arrSlotsPlace.Length; i++)
+            public GameObject objectItem;
+            public int id;
+        }
+
+
+        private int currentIdItem;
+        public int compareIdItem;
+        
+        public Item[] items;
+
+        private J_Inventory inventoryPlayer;
+
+        public void SetInventoryPlayer(J_Inventory value) => inventoryPlayer = value;
+
+        public Transform transformSpawnItem;
+
+        public void SpawnObject(int id, GameObject itemPlayer)
+        {
+            for(int i = 0; i < items.Length; i++)
             {
-                if (arrSlotsPlace[i].visibleObject.activeSelf)
+                if (id == items[i].id_item)
                 {
-                    tokenActivated = arrSlotsPlace[i].visibleObject;
-                    i = arrSlotsPlace.Length;
-                }
-            }
-            for (int i = 0; i < arrSlotsPlace.Length; i++)
-            {
-                if (arrSlotsPlace[i].Id_Object == id)
-                {
-                    bool visible = !arrSlotsPlace[i].visibleObject.activeSelf;
-                    bool changeToken = (tokenActivated != arrSlotsPlace[i].visibleObject && arrSlotsPlace[i].visibleObject);
-                    if (visible || changeToken)
-                    {
-                        currentIndexVisibleObjectOn = i;
-                        arrSlotsPlace[i].visibleObject.SetActive(true);
-                        arrSlotsPlace[i].transparentObject.SetActive(false);
-                    }
-                    else
-                    {
-                        arrSlotsPlace[i].visibleObject.SetActive(false);
-                        arrSlotsPlace[i].transparentObject.SetActive(true);
-                    }
-                }
-                else
-                {
-                    arrSlotsPlace[i].visibleObject.SetActive(false);
+                    items[i].transparentObject.SetActive(false);
+                    spawnedItem.objectItem = Instantiate(items[i].itemObject, transformSpawnItem.position, transformSpawnItem.rotation, transformSpawnItem);
+                    spawnedItem.objectItem.transform.localScale = new Vector3(1, 1, 1);
+                    spawnedItem.id = id;
+                    itemPlayer.SetActive(false);
+
+                    inventoryPlayer.RemoveItem(items[i].item);
+
+                    items[i].itemSpawned = true;
+                    currentIdItem = items[i].id_item;
+                    itemPlace = true;
+                    i = items.Length;
                 }
             }
         }
-        
+
+        public void DestroyObject(int id, GameObject itemPlayer)
+        {
+            if (spawnedItem != null)
+            {
+                for (int i = 0; i < items.Length; i++)
+                {
+                    if (id == items[i].id_item)
+                    {
+                        items[i].transparentObject.SetActive(true);
+
+                        itemPlayer.SetActive(true);
+                        inventoryPlayer.AddItem(items[i].item);
+
+                        items[i].itemSpawned = false;
+                        itemPlace = false;
+                        i = items.Length;
+                    }
+                }
+                Destroy(spawnedItem.objectItem);
+            }
+        }
     }
 
     [System.Serializable]
-    public class ItemsPlayer
+    public class ItemPlayer
     {
-        public GameObject objectItem;
-        public int Id_Object;
+        public GameObject go;
+        public int id;
     }
 
-    [SerializeField] private ObjectPlace[] answerObjectsPlace;
-    [SerializeField] private ItemsPlayer[] itemsPlayer;
+    public SpawnerItem[] spawnersItems;
+    public ItemPlayer[] itemsPlayer;
 
-    [SerializeField] private UnityEvent eventOnCorrectPlaceObject;
+    [SerializeField] private int[] answerPuzzleIds;
 
-    private int currentIndexObjectPlace = -1;
+    [SerializeField] private UnityEvent eventOnCorrectPlaceObjects;
 
-    public void SetCurrentIndexObjectPlace(int value) => currentIndexObjectPlace = value;
-
-    public void AddOrRemoveObjectPlace()
+    private void Start()
     {
-        int index = -1;
-       
+        for (int i = 0; i < spawnersItems.Length; i++)
+        {
+            spawnersItems[i].SetInventoryPlayer(inventory);
+            spawnersItems[i].spawnedItem = new SpawnerItem.SpawnedItem();
+        }
+    }
+
+    public void AddOrRemoveObject(int indexSpawnItems)
+    {
+        int indexItemPlayer = -1;
+
         for (int i = 0; i < itemsPlayer.Length; i++)
         {
-            if (itemsPlayer[i].objectItem.activeSelf)
+            if (itemsPlayer[i].go.activeSelf)
             {
-                index = i;
+                indexItemPlayer = i;
                 i = itemsPlayer.Length;
             }
         }
 
-        if (index == -1 || currentIndexObjectPlace < 0 || currentIndexObjectPlace >= answerObjectsPlace.Length)
+        if(spawnersItems[indexSpawnItems].itemPlace)
+        {
+            for (int i = 0; i < itemsPlayer.Length; i++)
+            {
+                if (itemsPlayer[i].id == spawnersItems[indexSpawnItems].spawnedItem.id)
+                {
+                    spawnersItems[indexSpawnItems].DestroyObject(itemsPlayer[i].id, itemsPlayer[i].go);
+                    i = itemsPlayer.Length;
+                }
+            }
+        }
+
+        if (indexItemPlayer == -1)
             return;
 
-        answerObjectsPlace[currentIndexObjectPlace].SwitchObjectVisible(itemsPlayer[index].Id_Object);
-        
+        for (int i = 0; i < spawnersItems[indexSpawnItems].items.Length; i++)
+        {
+            if (itemsPlayer[indexItemPlayer].id == spawnersItems[indexSpawnItems].items[i].id_item)
+            {
+                if (!spawnersItems[indexSpawnItems].items[i].itemSpawned)
+                {
+                    spawnersItems[indexSpawnItems].SpawnObject(itemsPlayer[indexItemPlayer].id, itemsPlayer[indexItemPlayer].go);
+                }
+            }
+        }
+
+        if (useCheckInPlaceObject)
+        {
+            CheckEventOnPlaceObject();
+        }
+
     }
+
+    public void CheckEventOnPlaceObject()
+    {
+        bool enableEvent = true;
+        for (int i = 0; i < answerPuzzleIds.Length; i++)
+        {
+            if (answerPuzzleIds[i] != spawnersItems[i].spawnedItem.id)
+            {
+                enableEvent = false;
+                i = answerPuzzleIds.Length;
+            }
+        }
+
+        if (enableEvent)
+        {
+            Debug.Log("Llegue al final del puzzle solo falta incorporar una funcion perri");
+            eventOnCorrectPlaceObjects?.Invoke();
+            enabled = false;
+        }
+
+    }
+
 }

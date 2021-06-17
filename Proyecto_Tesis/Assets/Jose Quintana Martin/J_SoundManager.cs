5a23082135c2a433edf45809d2876bc78aa080f7
José Quintana Martín <jquintanamartin@hotmail.com>
2021-06-17 18:37:14 -0300

    Book pickup sound implemented

---
+++ Proyecto_Tesis/Assets/Jose Quintana Martin/J_SoundManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class J_SoundManager : MonoBehaviour
+{
+    public void PlayEvent(string eventPath)
+    {
+        FMOD.Studio.EventInstance instance;
+        instance = FMODUnity.RuntimeManager.CreateInstance(eventPath);
+
+        instance.start();
+        instance.release();
+    }
+}

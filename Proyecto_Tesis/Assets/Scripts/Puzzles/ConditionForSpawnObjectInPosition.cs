cae12d72e6e24d73bf7f3cdf53e1f12271856843
TheoLeyenda <31700515+TheoLeyenda@users.noreply.github.com>
2021-06-18 13:26:50 -0300

    Incorporada la llave de arte que aparece al lado de la trompeta

---
+++ Proyecto_Tesis/Assets/Scripts/Puzzles/ConditionForSpawnObjectInPosition.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ConditionForSpawnObjectInPosition : MonoBehaviour
+{
+    [SerializeField] private J_Item objectRequire;
+    [SerializeField] private GameObject objectSpawn;
+    [SerializeField] private Transform spawn;
+    [SerializeField] private J_Inventory inventoryPlayer;
+    [SerializeField] private bool useParent;
+    [SerializeField] private UnityEvent OnGiveObject;
+
+    public void CheckGiveMyObject()
+    {
+        if (inventoryPlayer.PlayerHasItem(objectRequire))
+        {
+            inventoryPlayer.RemoveItem(objectRequire);
+
+            if (!useParent)
+                Instantiate(objectSpawn, spawn.position, spawn.rotation);
+            else
+                Instantiate(objectSpawn, spawn.position, spawn.rotation, spawn);
+
+            OnGiveObject?.Invoke();
+        }
+    }
+}

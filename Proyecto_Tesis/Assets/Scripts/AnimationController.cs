532088fb7c073c0143c17249e7c1fd0f18301022
TheoLeyenda <31700515+TheoLeyenda@users.noreply.github.com>
2021-06-18 04:12:14 -0300

    Incorporacion de arte de sebas en progreso...

---
+++ Proyecto_Tesis/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    public void PlayAnimation(string nameAnimation)
+    {
+        animator.Play(nameAnimation);
+    }
+
+    public void ChangeBuleanTrue(string name)
+    {
+        animator.SetBool(name, true);
+    }
+
+    public void ChangeBuleanFalse(string name)
+    {
+        animator.SetBool(name, false);
+    }
+}

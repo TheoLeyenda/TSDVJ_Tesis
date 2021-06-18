ef99e54df8d9b29aeb78c1deacc1e0d0658c878a
José Quintana Martín <jquintanamartin@hotmail.com>
2021-06-18 16:43:43 -0300

    Mouse now shows up

--- Proyecto_Tesis/Assets/Scripts/Manager/MouseManager.cs
+++ Proyecto_Tesis/Assets/Scripts/Manager/MouseManager.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class MouseManager : MonoBehaviour
 {
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
     public void SetCursorLockState(bool activate)
     {
         if (activate)
+        {
             Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
         else
+        {
             Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
     }
 }

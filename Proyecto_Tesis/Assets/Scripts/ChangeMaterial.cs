f224e363db045f857089c77d3f2c596eabe011f4
TheoLeyenda <31700515+TheoLeyenda@users.noreply.github.com>
2021-06-19 03:15:15 -0300

    Incorporacion de arte.

---
+++ Proyecto_Tesis/Assets/Scripts/ChangeMaterial.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMaterial : MonoBehaviour
+{
+    [SerializeField] private Material[] materials;
+
+    [SerializeField] private MeshRenderer meshRenderer;
+
+    public void ChangeMat(int indexMatAssigned)
+    {
+        if (indexMatAssigned < 0 || indexMatAssigned >= materials.Length)
+            return;
+
+        meshRenderer.material = materials[indexMatAssigned];
+    }
+}

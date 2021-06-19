3dda937af9f660123a40e59a1f6c416c84431b92
TheoLeyenda <31700515+TheoLeyenda@users.noreply.github.com>
2021-06-19 16:20:54 -0300

    .

---
+++ Proyecto_Tesis/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField] private List<Camera> camerasInGame;
+
+    public void SwitchCamera(int index)
+    {
+        if (index < 0 || index >= camerasInGame.Count)
+            return;
+
+        for (int i = 0; i < camerasInGame.Count; i++)
+        {
+            camerasInGame[i].gameObject.SetActive(false);
+        }
+
+        camerasInGame[index].gameObject.SetActive(true);
+
+    }
+    public void OnCameraForIndex(int index)
+    {
+        if (index < 0 || index >= camerasInGame.Count)
+            return;
+
+        camerasInGame[index].gameObject.SetActive(true);
+
+    }
+    public void OffCameraForIndex(int index)
+    {
+        if (index < 0 || index >= camerasInGame.Count)
+            return;
+
+        camerasInGame[index].gameObject.SetActive(false);
+    }
+
+}

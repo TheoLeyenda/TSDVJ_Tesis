8bfbdf1c27eda0442bd64a4898d8da438a625865
José Quintana Martín <jquintanamartin@hotmail.com>
2021-06-24 23:11:20 -0300

    Added resto de los sonidos xd (Tambien borre proyecto fmod)

--- Proyecto_Tesis/Assets/Jose Quintana Martin/J_Interactable.cs
+++ Proyecto_Tesis/Assets/Jose Quintana Martin/J_Interactable.cs
@@ -7,11 +7,27 @@ public class J_Interactable : MonoBehaviour
 {
     [SerializeField] private UnityEvent action;
 
+    [FMODUnity.EventRef]
+    public string interactionEvent;
+
+    [FMODUnity.ParamRef]
+    public string paramRef;
+
     void Start() { }
 
     public void DoAction()
     {
         if(enabled)
             action.Invoke();
+
+        //audio
+        if (interactionEvent != "")
+        {
+            J_SoundManager _SoundManager = FindObjectOfType<J_SoundManager>();
+
+            if (_SoundManager != null)
+                _SoundManager.PlayEvent(interactionEvent, gameObject, paramRef);
+        }
+        //audio
     }
 }

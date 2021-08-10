4fd41a7170fea15a769fdff5caa358680da862b1
TheoLeyenda <31700515+TheoLeyenda@users.noreply.github.com>
2021-08-10 03:27:32 -0300

    Bajando calidad grafica a las texturas

--- Proyecto_Tesis/Assets/Scripts/ConfigGame.cs
+++ Proyecto_Tesis/Assets/Scripts/ConfigGame.cs
@@ -7,7 +7,7 @@ public class ConfigGame : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        QualitySettings.vSyncCount = 0;
+        QualitySettings.vSyncCount = 1;
         Application.targetFrameRate = 60;
     }
 }

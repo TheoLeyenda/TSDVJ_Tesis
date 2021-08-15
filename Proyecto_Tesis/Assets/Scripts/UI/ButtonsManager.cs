a8cbd2a7f42a5d37a35b5cffb64379788a921ee3
TheoLeyenda <31700515+TheoLeyenda@users.noreply.github.com>
2021-08-14 21:45:10 -0300

    Flow del menu principal terminado.

--- Proyecto_Tesis/Assets/Scripts/UI/ButtonsManager.cs
+++ Proyecto_Tesis/Assets/Scripts/UI/ButtonsManager.cs
@@ -6,14 +6,16 @@ using UnityEngine.EventSystems;
 public class ButtonsManager : Updateable
 {
     public EventSystem eventSystem;
-    [SerializeField] private List<Button> buttons;
-    private Button currentButtonSelect;
-    private Button lastButtonSelect;
+    [SerializeField] private List<Selectable> buttons;
+
+    private Selectable currentButtonSelect;
+    private Selectable lastButtonSelect;
     // Start is called before the first frame update
     protected override void Start()
     {
         lastButtonSelect = null;
-        currentButtonSelect = buttons[0];
+        if(currentButtonSelect != null)
+            currentButtonSelect = buttons[0];
         base.Start();
         MyUpdate.AddListener(UpdateButtonsManager);
         UM.UpdatesInGame.Add(MyUpdate);
@@ -22,7 +24,9 @@ public class ButtonsManager : Updateable
     // Update is called once per frame
     void UpdateButtonsManager()
     {
-        
+        if (buttons.Count <= 0 || eventSystem == null)
+            return;
+
         for (int i = 0; i < buttons.Count; i++)
         {
             if (buttons[i].interactable)

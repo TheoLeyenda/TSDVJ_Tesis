1576932c23fe7dbb39172e7e68719e44ef8ecd7d
theixgamer <theoleyenda1997@gmail.com>
2021-08-23 18:57:11 -0300

    Revert "Merge remote-tracking branch 'origin/JoseQuintanaMartin' into TheoLeyenda"
    This reverts commit 195b95904ffef49f822e82417447b8238eac1edb, reversing
    changes made to cafaa8b41b9f8033f565bf029342fb9ca685c8df.

--- Proyecto_Tesis/Assets/Jose Quintana Martin/J_SaveSystem/J_PuzzleOnLoadActions.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Events;
-
-public class J_PuzzleOnLoadActions : MonoBehaviour
-{
-    public UnityEvent ActionPuzzle6;
-}

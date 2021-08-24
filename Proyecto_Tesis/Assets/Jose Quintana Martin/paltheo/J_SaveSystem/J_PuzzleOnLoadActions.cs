546e5f4cae3292ce0d476a2f5ef05f11102676fd
theixgamer <theoleyenda1997@gmail.com>
2021-08-24 00:00:08 -0300

    toma jose

---
+++ Proyecto_Tesis/Assets/Jose Quintana Martin/paltheo/J_SaveSystem/J_PuzzleOnLoadActions.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class J_PuzzleOnLoadActions : MonoBehaviour
+{
+    public UnityEvent ActionPuzzle6;
+}
